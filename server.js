ed55fff80a42199134e16c976c038062c51b1f32
Chu Song <chuchusong23@gmail.com>
2018-06-24 17:45:13 -0700

    Required path module in server file.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
 app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'index.html'));
